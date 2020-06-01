7022d85f9dfe4923e31a64e4ae1823d7fa8da5f6
Nour <alaran01@myunitec.ac.nz>
2020-06-01 16:12:34 +1200

    added Node class

--- GameStart.py
+++ GameStart.py
@@ -1,17 +1,57 @@
 import pyxel
 
+class Node:
+    def __init__(self, value, x, y):
+        self.left = None
+        self.right = None
+        self.value = value
+        self.x = x
+        self.y = y
+        self.radius = 8
+
+    def set_left(self, left):
+        self.left = left
+
+    def get_left(self):
+        return self.left
+
+    def set_right(self, right):
+        self.right = right
+
+    def get_right(self):
+        return self.right
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+    def get_value(self):
+        return self.value
+
+    def get_radius(self):
+        return self.radius
+
+
 class App:
     def __init__(self):
         pyxel.init(256, 180)
-        self.x = 0
-        self.y = 0
         pyxel.mouse(True)
         pyxel.load("assets/sound.pyxres")
+        self.nodes = []
+
+        n1 = Node(10, 120, 30)
+        n2 = Node(32, 60, 60)
+        n3 = Node(15, 30, 90)
+
+        self.nodes.append(n1)
+        self.nodes.append(n2)
+        self.nodes.append(n3)
+
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        self.x = (self.x + 1) % pyxel.width
-        self.y = (self.y + 1) % pyxel.height
         if(pyxel.btnp(pyxel.KEY_ENTER)):
             pyxel.play(0, 0)
         if (pyxel.btnp(pyxel.MOUSE_LEFT_BUTTON)):
@@ -31,13 +71,18 @@ class App:
         # Line at the bottom so I can print the text inside
         pyxel.line(0, 160, 256, 160, 8)
 
+        for node in self.nodes:
+            pyxel.circb(node.get_x(), node.get_y(), node.get_radius(), 8)
+            pyxel.text(node.get_x()-3, node.get_y()-2, str(node.get_value()), 8)
+
+
         # Drawing circle with text inside
         # Left hand side
-        pyxel.circb(120, 30, 8, 8)
-        pyxel.text(120, 28, "8", 8)
-
-        pyxel.circb(60, 60, 8, 8)
-        pyxel.text(60, 58, "3", 8)
+        # pyxel.circb(120, 30, 8, 8)
+        # pyxel.text(120, 28, "8", 8)
+        #
+        # pyxel.circb(60, 60, 8, 8)
+        # pyxel.text(60, 58, "3", 8)
 
         # pyxel.circb(120, 30, 8, 8)
         # pyxel.text(120, 28, "1", 8)
@@ -60,7 +105,6 @@ class App:
         #
         # pyxel.circb(120, 30, 8, 8)
         # pyxel.text(120, 28, "2", 8)
-
 
 
 App()
