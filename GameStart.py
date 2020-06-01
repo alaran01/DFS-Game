bdce3fe37979b52425a9d22bd1b96c179780e4c9
Nour <alaran01@myunitec.ac.nz>
2020-06-01 17:23:01 +1200

    created sample tree for reference and created the nodes for the tree

--- GameStart.py
+++ GameStart.py
@@ -41,13 +41,34 @@ class App:
         pyxel.load("assets/sound.pyxres")
         self.nodes = []
 
-        n1 = Node(10, 120, 30)
-        n2 = Node(32, 60, 60)
-        n3 = Node(15, 30, 90)
+        # tree
+        n1 = Node(35, 128, 30)
+        n2 = Node(15, 70, 65)
+        n3 = Node(45, 186, 65)
+        n4 = Node(13, 35, 100)
+        n5 = Node(19, 100, 100)
+        n6 = Node(42, 156, 100)
+        n7 = Node(55, 221, 100)
+        n8 = Node(10, 10, 135)
+        n9 = Node(14, 70, 135)
+        n10 = Node(39, 128, 135)
+        n11 = Node(50, 186, 135)
+        n12 = Node(59, 246, 135)
 
         self.nodes.append(n1)
         self.nodes.append(n2)
         self.nodes.append(n3)
+        self.nodes.append(n4)
+        self.nodes.append(n5)
+        self.nodes.append(n6)
+        self.nodes.append(n7)
+        self.nodes.append(n8)
+        self.nodes.append(n9)
+        self.nodes.append(n10)
+        self.nodes.append(n11)
+        self.nodes.append(n12)
+
+
 
         pyxel.run(self.update, self.draw)
 
@@ -74,37 +95,6 @@ class App:
         for node in self.nodes:
             pyxel.circb(node.get_x(), node.get_y(), node.get_radius(), 8)
             pyxel.text(node.get_x()-3, node.get_y()-2, str(node.get_value()), 8)
-
-
-        # Drawing circle with text inside
-        # Left hand side
-        # pyxel.circb(120, 30, 8, 8)
-        # pyxel.text(120, 28, "8", 8)
-        #
-        # pyxel.circb(60, 60, 8, 8)
-        # pyxel.text(60, 58, "3", 8)
-
-        # pyxel.circb(120, 30, 8, 8)
-        # pyxel.text(120, 28, "1", 8)
-        #
-        # pyxel.circb(120, 30, 8, 8)
-        # pyxel.text(120, 28, "5", 8)
-        #
-        # pyxel.circb(120, 30, 8, 8)
-        # pyxel.text(120, 28, "7", 8)
-        #
-        # pyxel.circb(120, 30, 8, 8)
-        # pyxel.text(120, 28, "4", 8)
-
-        # Right hand side
-        # pyxel.circb(120, 30, 8, 8)
-        # pyxel.text(120, 28, "6", 8)
-        #
-        # pyxel.circb(120, 30, 8, 8)
-        # pyxel.text(120, 28, "9", 8)
-        #
-        # pyxel.circb(120, 30, 8, 8)
-        # pyxel.text(120, 28, "2", 8)
 
 
 App()
