54710ac060c5f8f65cd93c46485ecc77103091ef
Nour <alaran01@myunitec.ac.nz>
2020-06-06 18:25:37 +1200

    Added game over and game won with music

--- GameStart.py
+++ GameStart.py
@@ -1,6 +1,7 @@
 import pyxel
 
 
+# Node class to creat a node
 class Node:
     def __init__(self, value, x, y):
         self.left = None
@@ -41,6 +42,7 @@ class Node:
     def get_selected(self):
         return self.selected
 
+    # check if clicked inside the node
     def inside(self, x, y):
         right = self.x + self.radius
         left = self.x - self.radius
@@ -58,13 +60,29 @@ class App:
         pyxel.mouse(True)
         pyxel.load("assets/sound_and_images.pyxres")
         pyxel.image(1).load(0, 0, "assets/game_over.png")
+        pyxel.image(2).load(0, 0, "assets/winner.jpg")
         self.nodes = []
         self.mode_pre_order = False
         self.mode_in_order = False
         self.mode_post_order = False
         self.list_of_orders = []
-        self.index_to_check = 0
+        self.index_to_check = -1
         self.num_of_lives = 3
+
+        self.start_music_game_over = False
+        self.start_music_game_won = False
+
+        pyxel.sound(6).set(
+            "r a1b1c2 b1b1c2d2 g2g2g2g2 c2c2d2e2" "f2f2f2e2 f2e2d2c2 d2d2d2d2 g2g2r r ",
+            "s",
+            "6",
+            "nnff vfff vvvv vfff svff vfff vvvv svnn",
+            25,
+        )
+
+        pyxel.sound(7).set(
+            "f0ra4r f0ra4r f0ra4r f0f0a4r", "n", "6622 6622 6622 6422", "f", 25
+        )
 
         # tree
         n1 = Node(35, 128, 30)
@@ -108,66 +126,82 @@ class App:
 
         pyxel.run(self.update, self.draw)
 
+    # reset the tree for a new game
+    def reset(self):
+        self.list_of_orders.clear()
+        self.index_to_check = 0
+        self.num_of_lives = 3
+        pyxel.play(0, 3)
+        for node in self.nodes:
+            node.set_selected(False)
+
     def update(self):
+
+        # checking if need to start game over music
+        if self.start_music_game_over:
+            pyxel.play(0, 6)
+            self.start_music_game_over = False
+
+        # checking if need to start game won music
+        if self.start_music_game_won:
+            pyxel.play(0, 7)
+            self.start_music_game_won = False
+
         if pyxel.btnp(pyxel.MOUSE_LEFT_BUTTON):
             x = pyxel.mouse_x
             y = pyxel.mouse_y
 
             # clicked pre-order button
             if x < 85 and y < 15:
-                self.list_of_orders.clear()
-                self.index_to_check = 0
+                self.reset()
                 self.mode_pre_order = True
                 self.mode_post_order = False
                 self.mode_in_order = False
                 self.pre_order(self.root)
-                pyxel.play(0, 3)
-
-                for node in self.nodes:
-                    node.set_selected(False)
 
             # clicked in-order button
             elif 85 < x < 170 and y < 15:
-                self.list_of_orders.clear()
-                self.index_to_check = 0
+                self.reset()
                 self.mode_in_order = True
                 self.mode_post_order = False
                 self.mode_pre_order = False
                 self.in_order(self.root)
-                pyxel.play(0, 3)
-
-                for node in self.nodes:
-                    node.set_selected(False)
 
             # clicked post-order button
             elif 170 < x and y < 15:
-                self.list_of_orders.clear()
-                self.index_to_check = 0
+                self.reset()
                 self.mode_post_order = True
                 self.mode_pre_order = False
                 self.mode_in_order = False
                 self.post_order(self.root)
-                pyxel.play(0, 3)
-
-                for node in self.nodes:
-                    node.set_selected(False)
 
             # check if clicked on a node
             for node in self.nodes:
                 if node.inside(x, y) and len(self.list_of_orders) != 0 and self.index_to_check < len(self.list_of_orders):
+
+                    # check if correct node
                     if node == self.list_of_orders[self.index_to_check]:
                         node.set_selected(True)
                         self.index_to_check += 1
                         pyxel.play(0, 4)
 
+                        # check if this was the last node needed
+                        if self.index_to_check >= len(self.list_of_orders):
+                            self.start_music_game_won = True
+
+                    # else wrong node
                     else:
                         pyxel.play(0, 0)
                         self.num_of_lives -= 1
 
+                        # check if this was the last live
+                        if self.num_of_lives == 0:
+                            self.start_music_game_over = True
+
     def draw(self):
         pyxel.cls(7)
 
-        # Menu to pick an order
+        # draw the navigation menu
         if self.mode_pre_order:
             pyxel.rect(0, 0, 85, 15, 8)
             pyxel.text(20, 5, "Pre-order", 7)
@@ -189,72 +223,77 @@ class App:
             pyxel.rectb(170, 0, 85, 15, 8)
             pyxel.text(190, 5, "Post-order", 8)
 
-        # drawing the nodes
-        for node in self.nodes:
-            if node.get_left() is not None:
-                left_child = node.get_left()
-                pyxel.line(node.get_x(), node.get_y(), left_child.get_x(), left_child.get_y(), 8)
+        # checking if game over
+        if self.num_of_lives <= 0:
+            # game over
+            pyxel.blt(78, 50, 1, 0, 0, 100, 53)
 
-            if node.get_right() is not None:
-                right_child = node.get_right()
-                pyxel.line(node.get_x(), node.get_y(), right_child.get_x(), right_child.get_y(), 8)
+        elif self.index_to_check == len(self.list_of_orders):
+            # game won
+            pyxel.blt(78, 50, 2, 0, 0, 100, 75)
 
-            if node.get_selected():
-                pyxel.circ(node.get_x(), node.get_y(), node.get_radius(), 8)
-                pyxel.text(node.get_x() - 3, node.get_y() - 2, str(node.get_value()), 7)
-
-            else:
-                pyxel.circ(node.get_x(), node.get_y(), node.get_radius(), 7)
-                pyxel.circb(node.get_x(), node.get_y(), node.get_radius(), 8)
-                pyxel.text(node.get_x() - 3, node.get_y() - 2, str(node.get_value()), 8)
-
-        # Line at the bottom so I can print the text inside
-        pyxel.line(0, 160, 256, 160, 8)
-
-        #  print the node value at the bottom when it is correct
-        x = 5
-        y = 168
-        for n in self.list_of_orders:
-            if n.get_selected():
-                pyxel.text(x, y, str(n.get_value()), 8)
-                x += 15
-        if self.index_to_check == len(self.list_of_orders) and len(self.list_of_orders) != 0:
-            pyxel.text(x, y, "Well done! ", 8)
-
-        # print instructions
-        if self.mode_in_order or self.mode_post_order or self.mode_pre_order:
-            if self.index_to_check == len(self.list_of_orders):
-                pyxel.text(5, 20, "Finished !!", 8)
-                pyxel.text(5, 30, "you won ! ", 8)
-            else:
+        else:
+            # print instructions
+            if self.mode_in_order or self.mode_post_order or self.mode_pre_order:
                 pyxel.text(5, 20, "Click on the nodes", 8)
                 pyxel.text(5, 30, "in the correct order ", 8)
-        else:
-            pyxel.text(5, 20, "Select one of ", 8)
-            pyxel.text(5, 30, "the above options", 8)
+            else:
+                pyxel.text(5, 20, "Select one of ", 8)
+                pyxel.text(5, 30, "the above options", 8)
 
-        # drawing hearts for lives
-        if self.num_of_lives == 0:
-            # pyxel.text(190, 30, "Game Over! ", 8)
-            pyxel.blt(100, 50, 1, 0, 0, 100, 53)
-        else:
+            # draw lives remaining
             pyxel.text(190, 20, "Lives remaining: ", 8)
             for i in range(self.num_of_lives):
                 pyxel.blt(200 + i * 18, 30, 0, 0, 0, 20, 20, 0)
 
+            # drawing the tree
+            for node in self.nodes:
 
+                # draw the line to the left child
+                if node.get_left() is not None:
+                    left_child = node.get_left()
+                    pyxel.line(node.get_x(), node.get_y(), left_child.get_x(), left_child.get_y(), 8)
+
+                # draw the line to the right child
+                if node.get_right() is not None:
+                    right_child = node.get_right()
+                    pyxel.line(node.get_x(), node.get_y(), right_child.get_x(), right_child.get_y(), 8)
+
+                # draw the node
+                if node.get_selected():
+                    pyxel.circ(node.get_x(), node.get_y(), node.get_radius(), 8)
+                    pyxel.text(node.get_x() - 3, node.get_y() - 2, str(node.get_value()), 7)
+                else:
+                    pyxel.circ(node.get_x(), node.get_y(), node.get_radius(), 7)
+                    pyxel.circb(node.get_x(), node.get_y(), node.get_radius(), 8)
+                    pyxel.text(node.get_x() - 3, node.get_y() - 2, str(node.get_value()), 8)
+
+            # Line at the bottom to print the text inside
+            pyxel.line(0, 160, 256, 160, 8)
+
+            #  print the node order at the bottom when they have been selected
+            x = 5
+            y = 168
+            for n in self.list_of_orders:
+                if n.get_selected():
+                    pyxel.text(x, y, str(n.get_value()), 8)
+                    x += 15
+
+    # find out the pre-order
     def pre_order(self, node):
         if node is not None:
             self.list_of_orders.append(node)
             self.pre_order(node.get_left())
             self.pre_order(node.get_right())
 
+    # find out the post-order
     def post_order(self, node):
         if node is not None:
             self.post_order(node.get_left())
             self.post_order(node.get_right())
             self.list_of_orders.append(node)
 
+    # find out the in-order
     def in_order(self, node):
         if node is not None:
             self.in_order(node.get_left())
